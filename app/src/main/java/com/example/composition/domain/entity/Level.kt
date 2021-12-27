9e41484d99c87a50dadb76182366728fc635f0ed
Azat Azmetov <azmetoff@gmail.com>
2021-12-27 12:19:24 +0300

    created layers with repository and usecases

---
+++ app/src/main/java/com/example/composition/domain/entity/Level.kt
@@ -0,0 +1,6 @@
+package com.example.composition.domain.entity
+
+enum class Level {
+
+    TEST, EASY, NORMAL, HARD
+}
---
+++ app/src/main/java/com/example/composition/domain/repository/GameRepository.kt
@@ -0,0 +1,16 @@
+package com.example.composition.domain.repository
+
+import com.example.composition.domain.entity.GameSettings
+import com.example.composition.domain.entity.Level
+import com.example.composition.domain.entity.Question
+
+interface GameRepository {
+
+    fun generateQuestion(
+        maxSumValue: Int,
+        countOfOptions: Int
+    ): Question
+
+    fun getGameSettongs(level: Level): GameSettings
+
+}
